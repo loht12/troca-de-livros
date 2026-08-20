9b67691efb87d9a942c25c26e26d641ff36b44aa
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-08-20 19:13:21 -0300

    add coisas

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-window.confirm("Voce pode se apresentar?")
+window.confirm("Voce pode se apresentar?");
 
-var nome = window.prompt("Qual e o seu nome?")
+var nome = window.prompt("Qual e o seu nome?");
+let idade = window.prompt("Qual e sua idade?");
+const genero = window.prompt("Qual e o seu genero?");
 
-window.alert(" Satisfacao em te conhecer " + nome + "!")
+window.alert( "Satisfacao em te conhecer, " + nome + ", voce tem " + idade + " e é do genero "+genero+ "!");
+window.alert(`Satisfacao em te conhecer ${nome} !`);
