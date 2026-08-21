ecd56b55a788de88da1be60667e2a4ec5d6a18f6
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-08-20 21:16:37 -0300

    add

--- praticando.JS/home.js
+++ praticando.JS/home.js
@@ -17,7 +17,11 @@ btnTroca.addEventListener( "mouseout", () => {
     nomeSaida.color = " #000000";
 } );
 
-function lancar(){
-    nomeSaida.style.color = "#000000";
+// Trocando a cor da caixa 
 
-}
+const btnVerde = document.getAnimations("btn-verde");
+const btnAmarelo = document.getAnimations("btn-amarelo");
+const btnAzul = document.getAnimations("btn-azul");
+const caixa = document.getAnimations("caixa");
+
+btnVerde.addEventListener("click, function()")
