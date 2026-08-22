2e5c9d56bddfcba54a69a3e6d4b6c087bacc2a95
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-08-21 21:14:13 -0300

    add coisas

--- praticando.JS/home.js
+++ praticando.JS/home.js
@@ -1,27 +1,64 @@
-
 const nomeEntrada = document.getElementById("nome-entrada");
 const nomeSaida = document.getElementById("nome-saida");
 const btnTroca = document.getElementById("btn-troca");
 
-btnTroca.addEventListener( "click", () => { 
+btnTroca.addEventListener('click', () => {
     nomeSaida.textContent = nomeEntrada.value;
-} );
+});
 
+btnTroca.addEventListener('mouseenter', function () {
+    nomeSaida.style.color = "#e7a8e8";
+});
 
-btnTroca.addEventListener( "mouseenter", () => { 
-    nomeSaida.style.color = " #8c9ced ";
-} );
+btnTroca.addEventListener('mouseout', () => {
+    nomeSaida.style.color = "#000000";
+});
 
+// Trocando a cor da caixa
 
-btnTroca.addEventListener( "mouseout", () => { 
-    nomeSaida.color = " #000000";
-} );
+const btnRoxo = document.getElementById("btn_roxo");
+const btnRoxo_clarinho = document.getElementById("btn_roxo_clarinho");
+const btnRoxinho = document.getElementById("btn_roxinho");
+const caixa = document.getElementById("caixa");
 
-// Trocando a cor da caixa 
+btnRoxo.addEventListener('click', () => {
+    caixa.style.backgroundColor = "rgb(179, 92, 181)";
+});
 
-const btnVerde = document.getAnimations("btn-verde");
-const btnAmarelo = document.getAnimations("btn-amarelo");
-const btnAzul = document.getAnimations("btn-azul");
-const caixa = document.getAnimations("caixa");
+btnRoxinho.addEventListener('click', () => {
+    caixa.style.backgroundColor = "rgb(253, 172, 252)";
+});
 
-btnVerde.addEventListener("click, function()")
+btnRoxo_clarinho.addEventListener('click', () => {
+    caixa.style.backgroundColor = "rgb(150, 107, 190)";
+});
+
+// Simulando adição e subtração de valor
+
+const valorReal = document.getElementById("valor_real");
+let contador = 0;
+
+document.getElementById("btn_menos").addEventListener('click', () => {
+    contador--;
+    valorReal.textContent = contador;
+})
+
+document.getElementById("btn_mais").addEventListener('click', () => {
+    contador++;
+    valorReal.textContent = contador;
+})
+
+//Adicionado livros
+
+const nomeLivro =document.getElementById("nome_livro");
+const listaLivros =document.getElementById("lista_livros")
+
+document.getElementById("adiciona_livro").addEventListener('click', ()=>{
+
+    const novolivro = document.createElement("li");
+    novolivro.textContent = nomeLivro.value;
+    listaLivros.appendChild(novolivro);
+
+    nomeLivro.value = "";
+
+});
--- praticando.JS/script.js
+++ praticando.JS/script.js
@@ -1,8 +1,9 @@
-window.confirm("Voce pode se apresentar?");
+window.confirm(" Voce quer se apresentar");
 
-var nome = window.prompt("Qual e o seu nome?");
-let idade = window.prompt("Qual e sua idade?");
-const genero = window.prompt("Qual e o seu genero?");
+var nome = window.prompt("Qual é o seu nome?");
+let idade = window.prompt('Qual a sua idade?');
+const genero =  window.prompt("Qual o seu genero?");
 
-window.alert( "Satisfacao em te conhecer, " + nome + ", voce tem " + idade + " e é do genero "+genero+ "!");
-window.alert(`Satisfacao em te conhecer ${nome} !`);
+window.alert( "Satisfação em te conhecer, "+ nome +", você tem "+ idade +" e é do genero "+genero+"!");
+
+window.alert(`Satisfação em te conhecer ${nome}, você tem ${idade} e é do gênero ${genero}!`)
